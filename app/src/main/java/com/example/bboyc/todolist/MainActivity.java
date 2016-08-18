2087331b76669264e7b114677804f978bd8f4cfc
Chris Rohr <chrisdrohr@gmail.com>
2016-08-18 12:06:43 -0700

    overhaul2

--- app/src/main/java/com/example/bboyc/todolist/MainActivity.java
+++ app/src/main/java/com/example/bboyc/todolist/MainActivity.java
@@ -64,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+
+//        int id = item.getItemId();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final EditText input = new EditText(this);
@@ -98,9 +99,9 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
