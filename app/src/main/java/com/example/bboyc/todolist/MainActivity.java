cd096737e080f7a7881a4d927cce1dcf99804de6
Chris Rohr <chrisdrohr@gmail.com>
2016-08-18 11:47:38 -0700

    overhaul

--- app/src/main/java/com/example/bboyc/todolist/MainActivity.java
+++ app/src/main/java/com/example/bboyc/todolist/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.bboyc.todolist;
-
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    TextView textView;
+
+    ArrayAdapter<String> arrayAdapter;
+
+    ArrayList<String> array = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listView = (ListView) findViewById(R.id.list_todo);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+
+        listView.setAdapter(arrayAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +65,37 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                builder.setView(input)
+
+                        .setMessage("New List")
+
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                array.add(input.getText().toString());
+                                arrayAdapter.notifyDataSetChanged();
+
+                            }
+                        })
+
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+
+
+        Dialog dialog = builder.create();
+        dialog.show();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
