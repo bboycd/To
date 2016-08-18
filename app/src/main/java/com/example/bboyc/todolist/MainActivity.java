c87ba20871559bdada8e05307d0c41a3b399086c
Chris Rohr <chrisdrohr@gmail.com>
2016-08-18 12:18:45 -0700

    overhaul3

--- app/src/main/java/com/example/bboyc/todolist/MainActivity.java
+++ app/src/main/java/com/example/bboyc/todolist/MainActivity.java
@@ -9,22 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-
     TextView textView;
-
     ArrayAdapter<String> arrayAdapter;
-
     ArrayList<String> array = new ArrayList<>();
 
     @Override
@@ -35,11 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         listView = (ListView) findViewById(R.id.list_todo);
-
         textView = (TextView) findViewById(R.id.textView);
-
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
-
         listView.setAdapter(arrayAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -50,59 +43,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-//        int id = item.getItemId();
-
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_TEXT);
 
-                builder.setView(input)
+        builder.setView(input)
 
-                        .setMessage("New List")
+                .setMessage("New List")
 
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
 
 
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                array.add(input.getText().toString());
-                                arrayAdapter.notifyDataSetChanged();
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        array.add(input.getText().toString());
+                        arrayAdapter.notifyDataSetChanged();
 
-                            }
-                        })
+                    }
+                })
 
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        });
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
 
 
         Dialog dialog = builder.create();
         dialog.show();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
+
